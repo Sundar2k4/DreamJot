3578291baf270e6b35db30695c7db38060d562f7
Sundar.c <csundar2004@gmail.com>
2025-11-18 12:02:54 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import home from "./components/home";
+const App = () => {
+  return;
+  <Router>
+    <Route path="/" element="<home/>" />
+  </Router>;
+};
+
+export default App;
